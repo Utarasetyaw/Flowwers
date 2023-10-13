61f49b92c53fe96fbca1a56f74d0b4358079cbfd
Utara.sw <tarazekage@gmail.com>
2023-10-14 01:07:48 +0700

    fix

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-onload = () =>{
-        document.body.classList.remove("container");
+onload = () => {
+  document.body.classList.remove("container");
+  window.addEventListener("DOMContentLoaded", (event) => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
+  });
 };
